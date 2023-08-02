744de97caf2f4e63f3d386d119ce0455a0926ab7
kcadesign <kace85@hotmail.com>
2023-08-02 20:00:23 +0900

    Added bouncy material to projectiles

--- Assets/_scripts/Enemies/ProjectileController.cs
+++ Assets/_scripts/Enemies/ProjectileController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5;
+    private Rigidbody2D _rigidBody;
+
+    //[SerializeField] private float _speed = 5;
 
     private Vector2 _movementDirection = Vector2.left;
 
@@ -15,6 +17,17 @@ public class ProjectileController : MonoBehaviour
         PlayerParry.OnParry += PlayerParry_OnParry;
     }
 
+    private void OnDisable()
+    {
+        PlayerParry.OnParry -= PlayerParry_OnParry;
+
+    }
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
     private void PlayerParry_OnParry()
     {
         _deflect = true;
@@ -22,25 +35,26 @@ public class ProjectileController : MonoBehaviour
 
     void Start()
     {
-        
+        _rigidBody.AddForce(_movementDirection, ForceMode2D.Impulse);
     }
 
     void Update()
     {
-        this.transform.Translate(_movementDirection * _speed * Time.deltaTime);
+        //this.transform.Translate(_movementDirection * _speed * Time.deltaTime);
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        print(collision.gameObject);
+        print(collision.gameObject.tag);
         if (collision.gameObject.CompareTag("Player") && _deflect)
         {
-            _movementDirection = Vector2.right;
+            //_movementDirection = Vector2.zero;
             Destroy(gameObject,3f);
         }
-        else if(collision.gameObject.CompareTag("Player") && !_deflect)
+        else
         {
             Destroy(gameObject);
         }
     }
+    
 }
